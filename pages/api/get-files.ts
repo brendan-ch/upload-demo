2d0602e769098c2e11f522efa8f8df88f2ae581e
Brendan Chen <smallpigeon1@gmail.com>
2023-02-07 15:18:23 -0800

    check if directory exists

--- pages/api/get-files.ts
+++ pages/api/get-files.ts
@@ -2,6 +2,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 import fs from 'fs';
 
 export default async function getFiles(req: NextApiRequest, res: NextApiResponse) {
+  if (!fs.existsSync('/tmp/upload-test')) {
+    fs.mkdirSync('/tmp/upload-test');
+  }
+
   const files = fs.readdirSync('/tmp/upload-test');
 
   return res.status(200).json({
