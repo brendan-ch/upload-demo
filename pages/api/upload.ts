3255655b7b3447a44b531df6d59b94dc05a6a8ee
Brendan Chen <smallpigeon1@gmail.com>
2023-02-07 15:09:25 -0800

    create a sub-directory

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -26,15 +26,19 @@ export default async function upload(req: NextApiRequest, res: NextApiResponse) 
   
   });  
 
+  if (!fs.existsSync('/tmp/upload-test')) {
+    fs.mkdirSync('/tmp/upload-test');
+  }
+
   const file = data.files.file as File;
   // file contains a path to the uploaded file
   console.log(file.filepath);
   const tempFile = fs.readFileSync(file.filepath);
-  fs.writeFileSync(`/tmp/${file.originalFilename}`, tempFile);
+  fs.writeFileSync(`/tmp/upload-test/${file.originalFilename}`, tempFile);
 
   // to indicate that it's working,
   // return a list of files on the server
-  const files = fs.readdirSync('/tmp');
+  const files = fs.readdirSync('/tmp/upload-test');
 
   return res.status(200).json({
     successful: true,
